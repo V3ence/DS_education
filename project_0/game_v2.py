c5629bf57de88ecf0dfb32531306e4331abc0ba4
V3ence <bubatobi@gmail.com>
2022-11-07 22:56:10 +1000

    111

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,38 +1,39 @@
 import numpy as np
 
 
-def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+def mid_predict(number):
+    """Угадываем число по алгоритму "среднего"
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        number (int, optional): Загаданное целое число. 
 
     Returns:
-        int: Число попыток
+        int: Число попыток.
     """
 
     count = 0
+    
+    # определяем границы интервала угадывания
     min_number = 0
     max_number = 100
 
     while True:
         count += 1
-        predict_number = round((min_number+max_number)/2) 
-        print (min_number, max_number, predict_number,number)
+        predict_number = round((min_number + max_number)/2) # вычисляем загаданное число методом "среднего"
         if number == predict_number:
             break # выход из цикла, если угадали
         elif number > predict_number:
-            min_number = predict_number
+            min_number = predict_number # уменьшаем интервал угадывания
         else:
-            max_number = predict_number
+            max_number = predict_number # уменьшаем интервал угадывания 
     return(count)
 
 
-def score_game(random_predict) -> int:
+def score_game(mid_predict):
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        mid_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
@@ -43,7 +44,7 @@ def score_game(random_predict) -> int:
     random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(mid_predict(number))
 
     score = int(np.mean(count_ls)) # находим среднее количество попыток
 
@@ -52,4 +53,4 @@ def score_game(random_predict) -> int:
 
 # RUN
 if __name__ == '__main__':
-    score_game(random_predict)
+    score_game(mid_predict)
