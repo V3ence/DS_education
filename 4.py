ee3ee73cb3048419509c8d2dd3849bbda9cc6c00
V3ence <bubatobi@gmail.com>
2022-12-12 21:45:59 +1000

    Version22.12.12

--- 4.py
+++ 4.py
@@ -1,47 +1,18 @@
 import pandas as pd
-
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-plt.style.use('default')
+sber_data = pd.read_csv('C:\IDE\sber_data.csv')
+fig, axes = plt.subplots(1, 2, figsize=(15, 4))
 
-covid_data = pd.read_csv('covid_data.csv')
-vaccinations_data = pd.read_csv('country_vaccinations.csv')
-vaccinations_data = vaccinations_data[
-    ['country', 'date', 'total_vaccinations', 
-     'people_vaccinated', 'people_vaccinated_per_hundred',
-     'people_fully_vaccinated', 'people_fully_vaccinated_per_hundred',
-     'daily_vaccinations', 'vaccines']
-]
-covid_data = covid_data.groupby(
-    ['date', 'country'], 
-    as_index=False
-)[['confirmed', 'deaths', 'recovered']].sum()
-covid_data['date'] = pd.to_datetime(covid_data['date'])
-covid_data['active'] = covid_data['confirmed'] - covid_data['deaths'] - covid_data['recovered']
-covid_data = covid_data.sort_values(by=['country', 'date'])
-covid_data['daily_confirmed'] = covid_data.groupby('country')['confirmed'].diff()
-covid_data['daily_deaths'] = covid_data.groupby('country')['deaths'].diff()
-covid_data['daily_recovered'] = covid_data.groupby('country')['recovered'].diff()
-vaccinations_data['date'] = pd.to_datetime(vaccinations_data['date'])
-covid_df = covid_data.merge(vaccinations_data, on=['date', 'country'], how='left')
+#гистограмма исходного признака
+histplot = sns.histplot(sber_data['mkad_km'], bins=30, ax=axes[0])
+histplot.set_title('MKAD Km Distribution');
 
-china_data = covid_data[covid_data['country'] == 'China']
-china_grouped = china_data.groupby(['date'])[['confirmed', 'active', 'deaths', 'recovered']].sum()
-
-#визуализация графиков
-fig = plt.figure(figsize=(10, 4))
-axes = fig.add_axes([0, 0, 1, 1])
-axes.plot(china_grouped['confirmed'], label='Общее число зафиксированных случаев', lw=3)
-axes.plot(china_grouped['deaths'], label='Общее число смертей', lw=3)
-axes.plot(china_grouped['recovered'], label='Общее число выздоровевших пациентов', lw=3)
-axes.plot(china_grouped['active'], label='Общее число активных случаев', lw=3, linestyle='dashed')
-
-#установка параметров отображения
-axes.set_title('Статистика Covid-19 в Китае', fontsize=16)
-axes.set_xlabel('Даты')
-axes.set_ylabel('Число случаев')
-axes.set_yticks(range(0, 100000, 10000))
-axes.xaxis.set_tick_params(rotation=30)
-axes.grid()
-axes.legend();
-plt.show()
+#гистограмма в логарифмическом масштабе
+log_mkad_km= np.log(sber_data['mkad_km'] + 1)
+histplot = sns.histplot(log_mkad_km , bins=30, ax=axes[1])
+histplot.set_title('Log MKAD Km Distribution');
--- 5.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-churn_data = pd.read_csv('C:\IDE\churn.csv')
-print(churn_data.head())
