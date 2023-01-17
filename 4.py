e7c72acbacbd667e61cc10318f010b95691d316f
V3ence <bubatobi@gmail.com>
2023-01-17 22:34:52 +1000

    last changes

--- 4.py
+++ 4.py
@@ -5,14 +5,62 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sber_data = pd.read_csv('C:\IDE\sber_data.csv')
-fig, axes = plt.subplots(1, 2, figsize=(15, 4))
+diabetes = pd.read_csv('diabetes_data.csv')
 
-#гистограмма исходного признака
-histplot = sns.histplot(sber_data['mkad_km'], bins=30, ax=axes[0])
-histplot.set_title('MKAD Km Distribution');
+duplicates = diabetes[diabetes.duplicated()]
+print('Число дубликтов: {}'.format(duplicates.shape[0]))
+diabetes = diabetes.drop_duplicates()
+print('Результирующее число записей: {}'.format(diabetes.shape[0]))
 
-#гистограмма в логарифмическом масштабе
-log_mkad_km= np.log(sber_data['mkad_km'] + 1)
-histplot = sns.histplot(log_mkad_km , bins=30, ax=axes[1])
-histplot.set_title('Log MKAD Km Distribution');
+def low_informative_feature(data):
+    low_information_cols = []
+    for col in data.columns:
+        top_freq = data[col].value_counts(normalize=True).max()
+        nunique_ratio = data[col].nunique() / data[col].count()
+        if top_freq > 0.95:
+            low_information_cols.append(col)
+        if nunique_ratio > 0.95:
+            low_information_cols.append(col)
+    return low_information_cols
+low_information_cols = low_informative_feature(diabetes)
+print(low_information_cols)
+
+def nan_function(x):
+    return np.nan if x == 0 else x
+diabetes["Glucose"] = diabetes["Glucose"].apply(nan_function)
+diabetes["BloodPressure"] = diabetes["BloodPressure"].apply(nan_function)
+diabetes["SkinThickness"] = diabetes["SkinThickness"].apply(nan_function)
+diabetes["Insulin"] = diabetes["Insulin"].apply(nan_function)
+diabetes["BMI"] = diabetes["BMI"].apply(nan_function)
+print(diabetes.isnull().mean().round(2).sort_values(ascending=False))
+
+thresh = diabetes.shape[0]*0.7
+diabetes = diabetes.dropna(thresh=thresh, axis=1)
+print(diabetes.shape[1])
+
+m = diabetes.shape[1]
+diabetes = diabetes.dropna(thresh=m-2, axis=0)
+print(diabetes.shape[0])
+
+null_data = diabetes.isnull().sum()
+cols = null_data[null_data>0].index
+for col in cols:
+    diabetes[col] = diabetes[col].fillna(diabetes[col].median())
+print(diabetes['SkinThickness'].mean().round(1))
+
+def outliers_iqr(data, feature, log_scale=False):
+    if log_scale:
+        x = np.log(data[feature])
+    else:
+        x = data[feature]
+    quartile_1, quartile_3 = x.quantile(0.25), x.quantile(0.75),
+    iqr = quartile_3 - quartile_1
+    lower_bound = quartile_1 - (iqr * 1.5)
+    upper_bound = quartile_3 + (iqr * 1.5)
+    outliers = data[(x<lower_bound) | (x > upper_bound)]
+    cleaned = data[(x>lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
+outliers, cleaned = outliers_iqr(diabetes, 'DiabetesPedigreeFunction')
+print(f'Число выбросов по методу Тьюки: {outliers.shape[0]}')
+print(f'Результирующее число записей: {cleaned.shape[0]}')
+
---
+++ 5.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+![img src=/design/piramida.jpg]()
